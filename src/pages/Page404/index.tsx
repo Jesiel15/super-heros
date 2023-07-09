1f96b69d979239b5dc302f44f246e3a54ede90ab
Jesiel Faria <fariajesiel@gmail.com>
2023-07-09 14:18:00 -0300

    Arrumando estilos

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
-import './style.scss'
-
+import "./style.scss";
 
 function Page404() {
   return (
-    <div className="container">
+    <div>
       <div className="page40">
-      <h1>Pagina 404</h1>
-      <Link to="/">
-        <button className="button-voltar">Voltar</button>
-      </Link>
-    </div>
+        <h1>Pagina 404</h1>
+        <Link to="/">
+          <button className="button-voltar">Voltar</button>
+        </Link>
+      </div>
     </div>
   );
 }
