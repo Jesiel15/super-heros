a5a478637ab091e4e9e29267ef8a22f37f172d2e
Jesiel Faria <fariajesiel@gmail.com>
2025-03-22 12:11:50 -0300

    Correção nos cards do heróis

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,45 +1,33 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.scss";
 
 function Card(props: any) {
   const navigate = useNavigate();
-  const [loading, setloading] = useState(false);
-
-  useEffect(() => {
-    var img = props.img;
-    const element = document.getElementById(props.id);
-    if (element) {
-      element.style.backgroundImage = "url(" + img + ")";
-    } else {
-      setloading(true);
-    }
-  });
 
   const editarHero = () => {
     navigate("/editar-hero", { state: props });
   };
 
   return (
-    <div>
-      {loading ? (
-        <>
-          <div className="card" id={props.id}>
-            <div className="card-content">
-              <h2 className="card-title">{props.name}</h2>
-              <p className="card-body">Descrição: {props.description}</p>
-              <p className="card-body">Poder: {props.power}</p>
-              <p className="card-body">Sexo: {props.sex}</p>
-              <p className="card-body">Origem: {props.origin}</p>
-              <a href="#" className="button" onClick={editarHero}>
-                Editar Herói
-              </a>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div>loading...</div>
-      )}
+    <div className="card">
+      {/* Imagem do herói */}
+      <div
+        className="card-img"
+        style={{ backgroundImage: `url(${props.img})` }}
+      ></div>
+
+      {/* Conteúdo do herói */}
+      <div className="card-content">
+        <h2 className="card-title">{props.name}</h2>
+        <p className="card-body">Descrição: {props.description}</p>
+        <p className="card-body">Poder: {props.power}</p>
+        <p className="card-body">Sexo: {props.sex}</p>
+        <p className="card-body">Origem: {props.origin}</p>
+
+        <a href="#" className="button" onClick={editarHero}>
+          Editar Herói
+        </a>
+      </div>
     </div>
   );
 }
