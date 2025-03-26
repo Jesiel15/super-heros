8d5171b3b079b53e5798f10b86889cc8683f43a9
Jesiel Faria <fariajesiel@gmail.com>
2025-03-26 15:43:10 -0300

    Alteração da porta do endpoint para AWS

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL:"https://ec2-52-14-153-181.us-east-2.compute.amazonaws.com:7000"
+  baseURL:"https://ec2-52-14-153-181.us-east-2.compute.amazonaws.com:443"
 })
 
 export default api;
